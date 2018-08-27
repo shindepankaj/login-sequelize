0bdfed030089c0cea06c7b36444920d55be1c188
KS101 <shinedpankaj1977@gmail.com>
2018-08-27 18:51:07 +0530

    Session maintained using JWT token.

--- app/controller/authenticate-controller.js
+++ app/controller/authenticate-controller.js
@@ -3,6 +3,8 @@ const User = db.users;
 
 const bcrypt = require('bcrypt');
 
+var jwt=require('jsonwebtoken');
+
 module.exports.authenticate=function(req,res){
     
     var email=req.body.email;
@@ -16,12 +18,17 @@ module.exports.authenticate=function(req,res){
             if(user){
 
                 bcrypt.compare(password, user.password, function(err, result) {
+                    
                     if(result) {
                         // Passwords match
+                        var token=jwt.sign(user.toJSON(),process.env.SECRET_KEY,{
+                            expiresIn:5000
+                        });
                         res.json({
                             status:true,
+                            token:token,
                             message:'successfully authenticated'
-                        });
+                        })
     
                     } else {
                         // Passwords don't match
